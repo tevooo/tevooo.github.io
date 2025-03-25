5f594f45629e9a59edfb427baf647483f7eaacd0
tevooo <69569336+tevooo@users.noreply.github.com>
2025-03-25 16:55:39 +0300

    script.js

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ const _slideTiga = function () {
     console.log("teks2 göründü");
     new TypeIt("#teks2", {
       strings: [
-        "sen benim için çok değerlisin bir o kadar da özelsin bende yerin o kadar farklı ki nasıl ifade edilir bilemiyorum telegramın bana bahşettiği en değerli şey sensin iyi ki varsın balım 🥳",
+        "sen benim için çok değerlisin bir o kadar da özelsin bende yerin o kadar farklı ki nasıl ifade edilir bilemiyorum... sen benim için her şeysin, hayatın bahşettiği en güzel şeysin iyi ki varsın kirazımmm 💝",
         " ",
         "her ne kadar aramızda ki bu iletişimin bir gün sona ereceği bilinci içinde olsakta son salisesine kadar seninle olmaktan, seninle konuşmaktan, seninle bir şeyler paylaşmaktan, senin yanında olmaktan mutluluk duyacağım 💞",
         " ",
